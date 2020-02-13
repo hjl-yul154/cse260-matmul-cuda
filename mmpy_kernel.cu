0d62fc12860f8cd877920805a843ab3d742ccff9
Xu Wei <xu.wei.31415@gmail.com>
2020-02-13 11:55:52 -0800

    shared block

--- mmpy_kernel.cu
+++ mmpy_kernel.cu
@@ -5,18 +5,42 @@
 #include "types.h"
 using namespace std;
 
-__global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B) {
+#define BLOCK_SIZE 32
 
-    int I =  blockIdx.y*blockDim.y + threadIdx.y;
-    int J =  blockIdx.x*blockDim.x + threadIdx.x;
+#define MAT_ELEMENT(mat, N, i, j) ((i)<N && (j)<N ? mat[(i)*N+(j)] : 0)
+#define A_ELEMENT(i, j) MAT_ELEMENT(A, N, i, j)
+#define B_ELEMENT(i, j) MAT_ELEMENT(B, N, i, j)
 
-    if((I < N) && (J < N)){
-        _DOUBLE_ _c = 0;
-        for (unsigned int k = 0; k < N; k++) {
-            _DOUBLE_ a = A[I * N + k];
-            _DOUBLE_ b = B[k * N + J];
-            _c += a * b;
-        }
-        C[I * N + J] = _c;
-    }
+__global__ void matMul(int N, _DOUBLE_* C, _DOUBLE_* A, _DOUBLE_* B) {
+	__shared__ _DOUBLE_ Ab[BLOCK_SIZE][BLOCK_SIZE];
+	__shared__ _DOUBLE_ Bb[BLOCK_SIZE][BLOCK_SIZE];
+
+	int bx = blockIdx.x, by = blockIdx.y;
+	int tx = threadIdx.x, ty = threadIdx.y;
+
+	_DOUBLE_ c = 0;
+
+	// Compute I,J of C
+	int I = bx * BLOCK_SIZE;
+	int J = by * BLOCK_SIZE;
+
+	for (int K = 0; K < N; K += BLOCK_SIZE) {
+		// load I,K of A
+		Ab[tx][ty] = A_ELEMENT(I + tx, K + ty);
+
+		// load K,J of B
+		Bb[tx][ty] = B_ELEMENT(K + tx, J + ty);
+
+		__syncthreads();
+
+		for (int k = 0; k < BLOCK_SIZE; ++k) {
+			c += Ab[tx][k] * Bb[k][ty];
+		}
+
+		__syncthreads();
+	}
+
+	if (I + tx < N && J + ty < N) {
+		C[(I + tx) * N + (J + ty)] = c;
+	}
 }
