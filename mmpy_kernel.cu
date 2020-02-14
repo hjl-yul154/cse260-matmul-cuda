03609da2fd9fea540c050f51b004f6c46d076165
Xu Wei <xu.wei.31415@gmail.com>
2020-02-13 17:47:13 -0800

    coalesce memory

--- mmpy_kernel.cu
+++ mmpy_kernel.cu
@@ -1,46 +1,48 @@
 // Matrix multiply device code
 #include <assert.h>
 #include <math.h>
-#include "utils.h"
 #include "types.h"
+#include "utils.h"
 using namespace std;
 
 #define BLOCK_SIZE 32
 
-#define MAT_ELEMENT(mat, N, i, j) ((i)<N && (j)<N ? mat[(i)*N+(j)] : 0)
-#define A_ELEMENT(i, j) MAT_ELEMENT(A, N, i, j)
-#define B_ELEMENT(i, j) MAT_ELEMENT(B, N, i, j)
+#define MAT(mat, N, i, j) (mat[(i)*N + (j)])
+#define MAT_PADDED(mat, N, i, j) ((i) < N && (j) < N ? MAT(mat, N, i, j) : 0)
+#define A_ELEMENT(i, j) MAT_PADDED(A, N, i, j)
+#define B_ELEMENT(i, j) MAT_PADDED(B, N, i, j)
+#define C_ELEMENT(i, j) MAT(C, N, i, j)
 
 __global__ void matMul(int N, _DOUBLE_* C, _DOUBLE_* A, _DOUBLE_* B) {
-	__shared__ _DOUBLE_ Ab[BLOCK_SIZE][BLOCK_SIZE];
-	__shared__ _DOUBLE_ Bb[BLOCK_SIZE][BLOCK_SIZE];
+    __shared__ _DOUBLE_ Ab[BLOCK_SIZE][BLOCK_SIZE];
+    __shared__ _DOUBLE_ Bb[BLOCK_SIZE][BLOCK_SIZE];
 
-	int bx = blockIdx.x, by = blockIdx.y;
-	int tx = threadIdx.x, ty = threadIdx.y;
+    int bx = blockIdx.x, by = blockIdx.y;
+    int tx = threadIdx.x, ty = threadIdx.y;
 
-	_DOUBLE_ c = 0;
+    _DOUBLE_ c = 0;
 
-	// Compute I,J of C
-	int I = bx * BLOCK_SIZE;
-	int J = by * BLOCK_SIZE;
+    // Compute I,J of C
+    int I = by * BLOCK_SIZE;
+    int J = bx * BLOCK_SIZE;
 
-	for (int K = 0; K < N; K += BLOCK_SIZE) {
-		// load I,K of A
-		Ab[tx][ty] = A_ELEMENT(I + tx, K + ty);
+    if (I + tx < N && J + ty < N) {
+        for (int K = 0; K < N; K += BLOCK_SIZE) {
+            // load I,K of A
+            Ab[ty][tx] = A_ELEMENT(I + ty, K + tx);
 
-		// load K,J of B
-		Bb[tx][ty] = B_ELEMENT(K + tx, J + ty);
+            // load K,J of B
+            Bb[ty][tx] = B_ELEMENT(K + ty, J + tx);
 
-		__syncthreads();
+            __syncthreads();
 
-		for (int k = 0; k < BLOCK_SIZE; ++k) {
-			c += Ab[tx][k] * Bb[k][ty];
-		}
+            for (int k = 0; k < BLOCK_SIZE; ++k) {
+                c += Ab[ty][k] * Bb[k][tx];
+            }
 
-		__syncthreads();
-	}
+            __syncthreads();
+        }
 
-	if (I + tx < N && J + ty < N) {
-		C[(I + tx) * N + (J + ty)] = c;
-	}
+        C_ELEMENT(I + ty, J + tx) = c;
+    }
 }
