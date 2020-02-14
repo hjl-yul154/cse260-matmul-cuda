c980c883bbf663d607b80f8540f6e854472a4ceb
Xu Wei <xu.wei.31415@gmail.com>
2020-02-13 22:00:58 -0800

    16x16 threads for 32*32 submatrix

--- mmpy_kernel.cu
+++ mmpy_kernel.cu
@@ -6,6 +6,12 @@
 using namespace std;
 
 #define BLOCK_SIZE 32
+#if BLOCK_SIZE % BLOCKDIM_X || BLOCK_SIZE % BLOCKDIM_Y
+#error BLOCK_SIZE must be multiple of blockDim
+#endif
+// Number of thread blocks in matrix block
+#define X_SUB (BLOCK_SIZE / BLOCKDIM_X)
+#define Y_SUB (BLOCK_SIZE / BLOCKDIM_Y)
 
 #define MAT(mat, N, i, j) (mat[(i)*N + (j)])
 #define MAT_PADDED(mat, N, i, j) ((i) < N && (j) < N ? MAT(mat, N, i, j) : 0)
@@ -20,29 +26,43 @@ __global__ void matMul(int N, _DOUBLE_* C, _DOUBLE_* A, _DOUBLE_* B) {
     int bx = blockIdx.x, by = blockIdx.y;
     int tx = threadIdx.x, ty = threadIdx.y;
 
-    _DOUBLE_ c = 0;
+    _DOUBLE_ c[Y_SUB][X_SUB] = {0}; // Zero initialize the whole array
 
-    // Compute I,J of C
-    int I = by * BLOCK_SIZE;
-    int J = bx * BLOCK_SIZE;
+    // Compute I0,J0 of C
+    int I0 = by * BLOCK_SIZE;
+    int J0 = bx * BLOCK_SIZE;
 
-    if (I + tx < N && J + ty < N) {
-        for (int K = 0; K < N; K += BLOCK_SIZE) {
-            // load I,K of A
-            Ab[ty][tx] = A_ELEMENT(I + ty, K + tx);
-
-            // load K,J of B
-            Bb[ty][tx] = B_ELEMENT(K + ty, J + tx);
-
-            __syncthreads();
-
-            for (int k = 0; k < BLOCK_SIZE; ++k) {
-                c += Ab[ty][k] * Bb[k][tx];
+    for (int K = 0; K < N; K += BLOCK_SIZE) {
+        for (int i = 0; i < BLOCK_SIZE; i += BLOCKDIM_Y) {
+            for (int j = 0; j < BLOCK_SIZE; j += BLOCKDIM_X) {
+                Ab[ty + i][tx + j] = A_ELEMENT(I0 + ty + i, K + tx + j);
             }
-
-            __syncthreads();
         }
 
-        C_ELEMENT(I + ty, J + tx) = c;
+        for (int i = 0; i < BLOCK_SIZE; i += BLOCKDIM_Y) {
+            for (int j = 0; j < BLOCK_SIZE; j += BLOCKDIM_X) {
+                Bb[ty + i][tx + j] = B_ELEMENT(K + ty + i, J0 + tx + j);
+            }
+        }
+
+        __syncthreads();
+
+        for (int k = 0; k < BLOCK_SIZE; ++k) {
+            for (int i = 0; i < Y_SUB; ++i) {
+                for (int j = 0; j < X_SUB; ++j) {
+                    c[i][j] += Ab[ty + i * BLOCKDIM_Y][k] * Bb[k][tx + j * BLOCKDIM_X];
+                }
+            }
+        }
+
+        __syncthreads();
+    }
+
+    for (int i = 0; i < Y_SUB; ++i) {
+        for (int j = 0; j < X_SUB; ++j) {
+            if (I0 + ty + i * BLOCKDIM_Y < N && J0 + tx + j * BLOCKDIM_X < N) {
+                C_ELEMENT(I0 + ty + i * BLOCKDIM_Y, J0 + tx + j * BLOCKDIM_X) = c[i][j];
+            }
+        }
     }
 }
